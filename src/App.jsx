261df1fdc7f04b5be7cfc2813e4e1325d4890832
Pawan Kumar Pandey <pawan4kp@gmail.com>
2026-04-26 02:38:53 +0530

    Version2 - useEffect explained - Service.jsx added

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,25 @@ import {NavLink,Routes, Route} from 'react-router-dom'
 import About from "./components/About"
 import Home from "./components/Home"
 import Show from "./components/Show"
+import Service from "./components/Service"
 function App() {
 
 
 
   return (
     <div >
-     <div className="flex items-center justify-center gap-5 text-xl"> 
+     <div className="flex items-center justify-center gap-5 text-xl py-5 bg-black text-white mb-10"> 
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/show">Show</NavLink>
+      <NavLink to="/service">Service</NavLink>
       </div>
 
     <Routes>
       <Route  path="/" element={<Home className/>}/>
       <Route path="/about" element={<About/>}/>
       <Route path="/show" element={<Show/>}/>
+      <Route path="/service" element={<Service/>}/>
     </Routes>
 
       
